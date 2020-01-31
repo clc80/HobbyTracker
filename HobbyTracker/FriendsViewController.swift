1bdd0c50333a5fb682527895c5e1fcf7634e3a85
Claudia Maciel <clc_80@yahoo.com>
2020-01-30 18:55:49 -0800

    changed filename

--- HobbyTracker/FriendsViewController.swift
+++ HobbyTracker/FriendsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FriendsViewController.swift
 //  HobbyTracker
 //
 //  Created by Claudia Contreras on 1/30/20.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FriendsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 

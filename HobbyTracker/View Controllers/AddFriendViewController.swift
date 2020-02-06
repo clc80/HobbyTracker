ed1154905ecd6313bec4d25eb56e4bd13af14d8a
Claudia Maciel <clc_80@yahoo.com>
2020-02-05 21:13:28 -0800

    App adds new row to table

--- HobbyTracker/View Controllers/AddFriendViewController.swift
+++ HobbyTracker/View Controllers/AddFriendViewController.swift
@@ -8,6 +8,19 @@
 
 import UIKit
 
+protocol AddFriendDelegate {
+    func friendWasCreated(_ friend: Friend)
+}
+
+//******************************************************************************
+//Delgator
+//1: Create a protocol with the delegate name
+//2: Create a delegate property of type (whatever your delgate name is )
+//3: When needed, call the delegate method on the delegator
+//******************************************************************************
+
+
+//Responsible for creating a friend object
 class AddFriendViewController: UIViewController {
     
     //MARK: - IBOutlet
@@ -17,6 +30,9 @@ class AddFriendViewController: UIViewController {
     @IBOutlet var hobby2TextField: UITextField!
     @IBOutlet var hobby3TextField: UITextField!
     
+    //MARK: - Private Propoerty
+    var delegate: AddFriendDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,8 +41,35 @@ class AddFriendViewController: UIViewController {
     
     //MARK: - IBActions
     @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func saveButtonTapped(_ sender: Any) {
-    }
+        guard let name = nameTextField.text,
+            let homeTown = homeTownTextField.text,
+            !name.isEmpty,
+            !homeTown.isEmpty else {return}
+        
+        var friend = Friend(name: name, hometown: homeTown, hobbies: [])
+        
+        if let hobby1 = hobby1TextField.text,
+            !hobby1.isEmpty {
+            friend.hobbies.append(hobby1)
+        }
+        
+        if let hobby2 = hobby2TextField.text,
+            !hobby2.isEmpty {
+            friend.hobbies.append(hobby2)
+        }
+        
+        if let hobby3 = hobby3TextField.text,
+                !hobby3.isEmpty {
+                friend.hobbies.append(hobby3)
+        }
+        
+        delegate?.friendWasCreated(friend)
+        self.dismiss(animated: true)
+        
+    } //end saveButtonTapped
+    
 }
--- HobbyTracker/View Controllers/FriendsTableViewController.swift
+++ HobbyTracker/View Controllers/FriendsTableViewController.swift
@@ -13,12 +13,55 @@ class FriendsTableViewController: UIViewController {
     //MARK: - IBOutlets
     @IBOutlet var tableView: UITableView!
     
-    
+    //MARK: - Properties
+    var friends: [Friend] = [Friend(name: "Claudia", hometown: "Chualar", hobbies: ["Reading", "Coding", "Walking"])]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
 
 }
 
+//MARK: = IBAction
+
+
+//MARK: - Extensions
+
+extension FriendsTableViewController: UITableViewDelegate {
+}
+
+extension FriendsTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friends.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "friendCell", for: indexPath) as? FriendTableViewCell else {return UITableViewCell()}
+        let friend = friends[indexPath.row]
+        cell.friend = friend
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "AddFriendSegue" {
+            let addFriendVC = segue.destination as! AddFriendViewController
+            
+            addFriendVC.delegate = self
+            
+        }
+    }
+}
+
+extension FriendsTableViewController: AddFriendDelegate{
+    func friendWasCreated(_ friend: Friend) {
+        self.friends.append(friend)
+        tableView.reloadData()
+    }
+}
+
+//Delegate
+//1. Adopt and conform to delegate/protocol
+//2. Set self as delegate when delegator is created

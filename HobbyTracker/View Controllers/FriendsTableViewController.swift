8fc39fef8cde1ed680ac0a0001d492b2aec41990
Claudia Maciel <clc_80@yahoo.com>
2020-02-06 17:05:38 -0800

    TableViewCell is now filled in with data

--- HobbyTracker/View Controllers/FriendsTableViewController.swift
+++ HobbyTracker/View Controllers/FriendsTableViewController.swift
@@ -14,11 +14,33 @@ class FriendsTableViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     
     //Table View Data Source
-    var friend: [Friend] = []
+    var friends: [Friend] = [Friend(name: "Claudia", hometown: "Chualar", hobbies: ["Reading", "Coding", "Walking"])]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
+}
+
+//MARK: - Extensions
+extension FriendsTableViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friends.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell", for: indexPath) as? FriendTableViewCell else { return UITableViewCell() }
+        
+        let friend = friends[indexPath.row]
+        
+        cell.nameLabel.text = friend.name
+        cell.hometownLabel.text = friend.hometown
+        cell.numOfHobbiesLabel.text = "\(friend.hobbies.count) hobbies"
+        
+        return cell
+    }
+    
+    
 }
